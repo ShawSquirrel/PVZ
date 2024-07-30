55242613895ecc5892a701e7cf175836dab4782c
莫罗-肖理旺 <xiaoliwang@holomatic.com>
2024-07-30 15:47:11 +0800

    Feat：Init.

---
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/GameApp_Main.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+using TEngine;
+
+public partial class GameApp
+{
+    private void AddLogicSystemMain()
+    {
+    }
+
+    private async UniTask StartMain()
+    {
+        GameModule.Timer.AddTimer(On3Second, 3f, true);
+        await UniTask.CompletedTask;
+    }
+
+    private void On3Second(object[] args)
+    {
+        Log.Info("On3Second");
+    }
+}
